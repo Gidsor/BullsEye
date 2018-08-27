776a8b7caea8c80b416ef68201ee9f0788abac05
Vadim Denisov <gidsor@gmail.com>
2018-08-27 23:43:58 +0500

    Fix slider

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = Int(slider.value.rounded())
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,10 +24,14 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Your score", message: "\(currentValue)", preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated:  true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = Int(slider.value.rounded())
     }
 }
 
